b0421d4ee4e31faa0976321954a92490aee31fd6
xueqiwei <xueqiwei@163.com>
2018-04-14 13:21:21 +0800

    方便展示的文本框组件

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
-$(function(){
-	$('#dowebok').fullpage({
-		'paddingTop':150,
-		'navigation': true,
-		afterLoad:function(anchorlink,index){
-			
-		}	
-	})
-
-	// $('.section').css({
-	// 	'backgroundSize':$(window).width()+'px' $(window).height()+'px'
-	// })
-
-
+var app = new Vue({
+	el: '#dowebok',
+	data: {
+		area: {
+			width: 450,
+			height: 200,
+			font: 14,
+			limit: 10,
+			text: '我是示例'
+		}
+	},
+	created: function () {
+		$('#dowebok').fullpage({
+			'paddingTop': 150,
+			'navigation': true,
+		})
+		$('#area1').tarea(this.area)
+	},
+	watch: {
+		area: {
+			handler: function (val, oldval) {
+				$('#area1').html('').tarea(val)
+			},
+			deep: true
+		}
+		
+	}
 })
