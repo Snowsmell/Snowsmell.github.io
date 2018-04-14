0e89b9f307b5ff2c9c48767cd16d806e421c9ce1
xueqiwei <xueqiwei@163.com>
2018-04-14 15:55:49 +0800

    调整后的签字组件

--- js/index.js
+++ js/index.js
@@ -5,8 +5,27 @@ var app = new Vue({
 			width: 450,
 			height: 200,
 			font: 14,
-			limit: 10,
-			text: '我是示例'
+			limit: 200,
+			text: '我是示例',
+			color:"#333"
+		},
+		sign:{
+			width: 600,
+			height: 300,
+			border: 'none',
+			background: '#ccc',
+			lineWidth: 4,
+			lineColor: '#fff',
+		},
+		pic:[]
+	},
+	methods:{
+		clearsign:function(){
+			$('#sign1').sign('empty')
+		},
+		savepic:function(){
+			$('#sign1').sign('exportImg')
+			this.pic.push(sessionStorage.getItem('signImg'))
 		}
 	},
 	created: function () {
@@ -15,6 +34,7 @@ var app = new Vue({
 			'navigation': true,
 		})
 		$('#area1').tarea(this.area)
+		$('#sign1').sign(this.sign)
 	},
 	watch: {
 		area: {
@@ -22,7 +42,12 @@ var app = new Vue({
 				$('#area1').html('').tarea(val)
 			},
 			deep: true
+		},
+		sign: {
+			handler:function(val,oldval){
+				$('#sign1').html('').sign(val)
+			},
+			deep:true
 		}
-		
 	}
 })
